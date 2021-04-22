a2c3a22b7dd233f950bdfa363b72013f6e3f3c96
Eder_Ramos-Filho <eder31250@gmail.com>
2021-04-21 23:45:23 -0300

    Api's Included
    Dias 1, 2 e 3 da NLW#5

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavigationContainer } from "@react-navigation/native";
+
+import  StackRoutes  from "../routes/routes.stack"
+
+const Routes = () => (
+  <NavigationContainer>
+    < StackRoutes />
+  </NavigationContainer>
+)
+
+export default Routes
